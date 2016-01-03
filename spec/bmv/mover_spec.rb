f3b08756a0d1dcb23c3da0c30c064c7358208512
Monty Hindman <mhindman@apple.com>
2016-01-02 20:28:07 -0800

    End-to-end test: partial progress on dirtree helpers

--- spec/bmv/mover_spec.rb
+++ spec/bmv/mover_spec.rb
@@ -2,12 +2,72 @@ require 'spec_helper'
 
 describe Bmv::Mover do
 
-  let(:x) { 42 }
+  # TODO: maybe use around(:example)
+  #
+  # around(:example) do
+  #   Dir.chdir(WORK_AREA_ROOT) do
+  #     clear_work_area
+  #     yield
+  #     clear_work_area
+  #   end
+  # end
+  #
+  # That will allow the bmv runs to be written naturally, without
+  # worrying about WORK_AREA_ROOT at all.
+  #
+  # If I make this change, adjust create_work_area() accordingly.
+  #   - if cwd is already WORK_AREA_ROOT, don't add prefix
 
-  context 'todo' do
+  before(:example) {
+    clear_work_area
+  }
+
+  after(:example) {
+    clear_work_area
+  }
+
+  let(:ex1) {
+    %q{
+      d1/
+      d1/f1
+      d1/f2.txt
+
+      d2/
+      d2/f3.txt
+      d2/f4.mp3
+      d2/f5.mp3
+
+      d3/
+      d3/f6.txt
+      d3/f7a.txt
+      d3/f7b.mp3
+    }
+  }
+
+  let(:ex1_parsed) {
+    %w{
+      d1/
+      d1/f1
+      d1/f2.txt
+      d2/
+      d2/f3.txt
+      d2/f4.mp3
+      d2/f5.mp3
+      d3/
+      d3/f6.txt
+      d3/f7a.txt
+      d3/f7b.mp3
+    }
+  }
+
+  context 'scenarios' do
+
+    it '#parse_work_area_text' do
+      expect(parse_work_area_text(ex1)).to eql(ex1_parsed)
+    end
 
     it 'todo' do
-      expect(x).to eql(42)
+      # create_work_area(ex1)
     end
 
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,42 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
 require 'bmv'
+require 'fileutils'
+
+
+module Bmv
+  module SpecHelpers
+
+    # TODO: define this relative the to path of the current file.
+    WORK_AREA_ROOT = 'spec/work_area'
+
+    def parse_work_area_text(txt)
+      # Text some text and parses into striped, non-empty lines.
+      txt.split("\n").map(&:strip).reject(&:empty?)
+    end
+
+    def create_work_area(txt)
+      # Creates a directory tree in the testing work area.
+      parse_work_area_text(txt).each { |line|
+        path = File.join(WORK_AREA_ROOT, line)
+        if path.end_with?('/')
+          FileUtils::mkdir_p(path.chop)
+        else
+          FileUtils::touch(path)
+        end
+      }
+    end
+
+    def clear_work_area
+      # Deletes the testing work area.
+      FileUtils::rm_rf(WORK_AREA_ROOT)
+    end
+
+  end
+end
+
+
+RSpec.configure do |c|
+  c.include Bmv::SpecHelpers
+end
 
