4bd4c165ce405fc0c1cfe9c58ccc861060a00754
Monty Hindman <mhindman@apple.com>
2022-12-21 23:58:17 -0800

    Moved Kwexception to different repo

--- src/bmv/utils.py
+++ src/bmv/utils.py
@@ -1,6 +1,11 @@
 import subprocess
 from dataclasses import dataclass
 
+from kwexception import Kwexception
+
+class BmvError(Kwexception):
+    pass
+
 def read_from_file(path):
     with open(path) as fh:
         return fh.read()
@@ -38,108 +43,4 @@ class RenamePair:
     @property
     def formatted(self):
         return f'{self.orig}\n{self.new}\n'
-
-class Kwexception(Exception):
-
-    '''
-
-    Constructor should take *xs, which is consistent with other Exception classes.
-
-    Avoid designing the class so that data in xs can be lost.
-
-    The purposes of Kwexception.new():
-
-        - Convert another error type to the error-type known by your project.
-          [see NEW_CONVERT]
-
-        - Augment a Kwexception instance with more keyword args, either in the
-          fashion of dict.update or dict.setdefault. [see NEW_UPDATE]
-
-        - Add some attributes from the initial Exception to the params. [see
-          NEW_INITIAL]
-
-        - But its purposes do not include, replacing or improving upon Python's
-          traceback generation or handling of __context__ and __cause__. Let
-          the user raise-with as needed.
-
-    Is STRINGIFY really needed? Defer for now.
-
-        There might be one valid use case: someone who wants stringification to
-        be just the self.msg (or less compellingly, self.params), but they need
-        the underlying self.args to be a tuple with multiple elements (maybe a
-        named tuple).
-
-        But this could be added later without changing anything else.
-
-    Some code:
-
-        MOVE = 'move'
-        COPY = 'copy'
-        MSG = 'msg'
-
-        SET_MSG = MOVE            # MOVE|COPY|None
-        SUPER_PARAMS = True       # .
-        NEW_CONVERT = True        # .
-        NEW_UPDATE = True         # If False, will use setdefault instead.
-        NEW_CONTEXT = True        # .
-
-        def __init__(self, *xs, **kws):
-
-            # Put the msg into kws, as the first key.
-            if xs and self.SET_MSG in (self.MOVE, self.COPY) and self.MSG not in kws:
-                d = {self.MSG: xs[0]}
-                d.update(kws)
-                kws = d
-                if self.SET_MSG == self.MOVE:
-                    xs = xs[1:]
-
-            # Add kws to xs so that it will be included in the super() call.
-            if self.SUPER_PARAMS:
-                xs = xs + (kws,)
-
-            # Set params and make the super() call.
-            self.params = kws
-            super().__init__(*xs)
-
-        @property
-        def msg(self):
-            return self.params.get(self.MSG, None)
-
-    '''
-
-    def __init__(self, msg = '', **kws):
-        d = {'msg': msg}
-        d.update(kws)
-        super(Kwexception, self).__init__(d)
-
-    @classmethod
-    def new(cls, e, **kws):
-        if isinstance(e, cls):
-            e.params.update(kws)
-            return e
-        else:
-            return cls(
-                # TODO: switch to these keywords:
-                # context_error = type(e).__name__,
-                # context_args = e.args,
-                # context_str = str(e),
-
-                orig_error = type(e).__name__,
-                orig_msg = str(e),
-                **kws,
-            )
-
-    def __str__(self):
-        return str(self.params)
-
-    @property
-    def params(self):
-        return self.args[0]
-
-    @property
-    def msg(self):
-        return self.params['msg']
-
-class BmvError(Kwexception):
-    pass
 
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,45 +1,9 @@
 import pytest
 
-from bmv.utils import BmvError
+from bmv.utils import RenamePair
 
-def test_bmv_error(tr):
-
-    # Helper to check BmvError instances.
-    def check(e, exp):
-        assert isinstance(e, BmvError)
-        assert e.params == exp
-        assert str(e) == str(exp)
-
-    # Some keyword arguments.
-    initial = {'msg': 'hello', 'a': 1, 'b': 2}
-    popped = dict(initial)
-    msg = popped.pop('msg')
-
-    # Basic usage: create a BmvError via keyword parameters.
-    e1 = BmvError(**initial)
-    check(e1, initial)
-
-    # Same scenario, but msg is given positionally.
-    e2 = BmvError(msg, **popped)
-    check(e2, initial)
-
-    # Create BmvError via the classmethod new(), with a ValueError.
-    kws = dict(msg = 'frobnosticate', a = 11, b = 22)
-    msg0 = 'blort blort'
-    e0 = ValueError(msg0)
-    exp = {
-        'orig_error': 'ValueError',
-        'orig_msg': msg0,
-        **kws,
-    }
-    e = BmvError.new(e0, **kws)
-    assert e.params == exp
-
-    # Create BmvError via the classmethod new(), with a BmvError.
-    kws = dict(msg = 'frobnosticate', a = 11, b = 22)
-    extra = dict(a = 9999, x = 3333, y = 2222)
-    e0 = BmvError(**kws)
-    e = BmvError.new(e0, **extra)
-    exp = {**kws, **extra}
-    assert e.params == exp
+def test_rename_pair(tr):
+    rp = RenamePair('a', 'b')
+    assert rp.orig == 'a'
+    assert rp.new == 'b'
 
